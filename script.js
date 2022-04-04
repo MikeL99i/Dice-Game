abd15f7fd2df0d772ce35cff8d00d7d4487e2df4
Mike Li <lim48687@gmail.com>
2022-04-04 19:11:04 +0200

    message display addded
    shows player turn and the winner

--- script.js
+++ script.js
@@ -9,10 +9,69 @@ const message = document.getElementById("message");
 
 const p1Message = "Player 1 Turn";
 const p2Message = "Player 2 Turn";
+const p1Victory = "Player 1 Won 🎉🎉🎉";
+const p2Victory = "Player2 Won 🎉🎉🎉";
 
 let windCondition = false;
 let p1Score = 0;
 let p2Score = 0;
 let p1Dice = 0;
 let p2Dice = 0;
+let playerTurn = 0; // Flag for tracking player turn =>  0 = Player 1 turn; 1 = Player 2 Turn
 
+rollBtn.addEventListener("click", ClickRoll)
+
+
+function ClickRoll() {
+    RollDice();
+    if(p1Score >= 20 || p2Score >= 20) {
+        windCondition = true;
+    }
+    if(playerTurn === 0) {
+        if(windCondition === false)
+            DisplayMessage(p1Message);
+        else
+            DisplayMessage(p2Victory);
+    }else {
+        if(windCondition === false)
+            DisplayMessage(p2Message);
+        else
+            DisplayMessage(p1Victory);
+    }
+}
+
+function DisplayMessage(theMessage) {
+    message.innerHTML = theMessage;
+}
+
+function RollDice() {
+    if(playerTurn === 0) {
+        p1Dice = RandomDice();
+        p1Score += p1Dice;
+        DisplayScoreAndDice(p1Dice, p1Score, playerTurn);
+        playerTurn = 1;
+    }else {
+        p2Dice = RandomDice();
+        p2Score += p2Dice;
+        DisplayScoreAndDice(p2Dice, p2Score, playerTurn);
+        playerTurn = 0;
+    }
+    if(windCondition === true) {
+
+    }
+}
+
+// Dice function retrun random number between 1 to 6
+function RandomDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+function DisplayScoreAndDice(dice, score, playerTurn) {
+    if(playerTurn === 0) {
+        p1Scoreboard.innerHTML = score;
+        p1DiceDisplay.innerHTML = dice;
+    }else {
+        p2Scoreboard.innerHTML = score;
+        p2DiceDisplay.innerHTML = dice;
+    }
+}
